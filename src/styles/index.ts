4bd122e6c05249d04ab8ca62c5cb55eefb3b58c4
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-28 11:23:20 -0300

    feat: configurando Stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6'
+    }
+  }
+});

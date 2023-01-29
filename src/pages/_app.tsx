b74bbde47c1c7ea5beea12d4fe85533f728f7bfa
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-29 16:47:22 -0300

    feat: cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '@/styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }

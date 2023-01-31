2911eb9146f06e09b9d6fa7cf5a004d28a722f9a
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-31 12:28:37 -0300

    feat: criando carrossel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,17 +2,18 @@ import { styled } from ".."
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
+  // gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1080px)/2))',
   marginLeft: 'auto',
   minHeight: 656,
+  overflow: 'hidden',
 });
 
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
+  // padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',

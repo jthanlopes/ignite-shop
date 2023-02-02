dadfc7141fd3d9eca38f8512d3086a46ca3d63eb
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-02-02 19:27:29 -0300

    feat: navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Stripe from "stripe"
 import { stripe } from "@/lib/stripe"
 
 import { HomeContainer, Product } from "@/styles/pages/home"
+import Link from "next/link"
 
 interface HomeProps {
   products: {
@@ -27,14 +28,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id} >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>

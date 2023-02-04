e4f0813dde9332da5cf7786f18fc8963e282c745
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-02-04 15:30:18 -0300

    feat: checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,6 +12,7 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
@@ -20,6 +21,10 @@ export default function Product({ product }: ProductProps) {
 
   if (isFallback) {
     return <p>Carregando...</p>
+  }
+
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId);
   }
 
   return (
@@ -34,7 +39,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -68,7 +73,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL',
         }).format(price.unit_amount! / 100),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hora

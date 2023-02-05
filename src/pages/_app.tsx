6f6e036a4f55b95062d38860ad68a6a996555300
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-02-05 20:40:24 -0300

    feat: estrutura da página de sucesso

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import logoImg from '@/assets/logo.svg'
 import Image from 'next/image'
 import { Container, Header } from '@/styles/pages/app'
+import Link from 'next/link'
 
 globalStyles()
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" />
+        </Link>
       </Header>
 
       <Component {...pageProps} />
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { SuccessContainer, ImageContainer } from "@/styles/pages/success"
+import Link from "next/link"
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuuul <strong>Jonathan Santos</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
